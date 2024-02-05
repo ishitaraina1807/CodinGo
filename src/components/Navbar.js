fdc92a8a208d81a5ed930e1cfd66213577a97214
ishitaraina1807 <ishitaraina18@gmail.com>
2024-02-05 21:32:33 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
                 <h1 className="text-[#5252D4] font-montserrat text-2xl font-bold leading-[1.309] tracking-[0.48]">CodinGo</h1>
             </div>
             <div className={`lg:flex text-white lg:flex-row top-12 lg:top-0 lg:items-center lg:justify-around lg:space-x-4 absolute left-0 right-0 bg-[#5252D4] lg:bg- z-50 ${isOpen ? 'flex flex-col' : 'hidden'} lg:relative lg:flex-row lg:visible w-full lg:z-0`}>
-                <p className='lg:py-0 py-2 lg:px-0 md:text-xl px-4 text-center lg:text-left'>Compile and Execute codes in 40+ languages</p>
+                <p className='lg:py-0 font-semibold py-2 lg:px-0 md:text-xl px-4 text-center lg:text-left'>Compile and Execute codes in 40+ languages</p>
             </div>
             <div className='lg:hidden absolute top-3 right-3' onClick={toggleMenu}>
                 <svg
