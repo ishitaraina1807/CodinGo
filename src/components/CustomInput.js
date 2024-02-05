691c11558a916500d9630a5fa0c73a8d58bc364a
ishitaraina1807 <ishitaraina18@gmail.com>
2024-02-05 21:29:58 +0530

    sutom input values

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -9,7 +9,7 @@ const CustomInput = ({ customInput, setCustomInput }) => {
         rows="5"
         value={customInput}
         onChange={(e) => setCustomInput(e.target.value)}
-        placeholder={`Custom input`}
+        placeholder={`Custom input...`}
         className={classnames(
           "focus:outline-none w-full border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white mt-2"
         )}
